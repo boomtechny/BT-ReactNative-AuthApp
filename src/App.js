78f56f71a235fb9001b4f39ccd31abf9dd041f60
BoomTech <boomtechny@gmail.com>
2018-03-01 17:56:47 -0500

    Cleared sensitive data from commits. Continued to add handling User Authentication State changes to handle rendering appropriately if user is logged in or logged out

--- src/App.js
+++ src/App.js
@@ -1,11 +1,15 @@
 import React, {Component} from 'react'; 
 import {View} from 'react-native'; 
 import * as firebase from 'firebase';
-import {Header} from './components/common';
+import { Header, Button } from './components/common';
 import LoginForm from './components/LoginForm';
 import  { FB_APIKEY, FB_AUTHDOMAIN, FB_DATABASEURL, FB_PROJECTID, FB_STORAGEBUCKET, FB_MESSAGINGSENDERID} from '../config/constants.js';
 
 class App extends Component{
+
+
+  state = {loggedIn :false }; 
+
 componentWillMount(){
   
  var app = firebase.initializeApp({
@@ -16,13 +20,33 @@ componentWillMount(){
     storageBucket: FB_STORAGEBUCKET,
     messagingSenderId: FB_MESSAGINGSENDERID
   });
+
+
+firebase.auth().onAuthStateChanged((user)=>{
+if(user){
+  this.setState({loggedIn:true}); 
+  }
+  else{
+    this.setState({loggedIn:false}); 
+  }
+}); 
+}
+renderContent(){
+  if(this.state.loggedIn){
+    return(
+      <Button>
+        Log Out 
+        </Button>
+    );
+  }
+  return <LoginForm/>
 }
 
   render(){
     return(
       <View>
         <Header headerText = "Authentication"/>
-        <LoginForm/>
+        {this.renderContent()}
         
         </View>
     );
