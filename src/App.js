6c77e7d90f58f44bd965f2011bab4ea7c1b6835f
BoomTech <boomtechny@gmail.com>
2018-03-01 13:18:15 -0500

    Integrated firebase for authentication of user with email and password after Log In button press,  including authentication error response

--- src/App.js
+++ src/App.js
@@ -1,9 +1,15 @@
 import React, {Component} from 'react'; 
 import {View} from 'react-native'; 
+import * as firebase from 'firebase';
 import {Header} from './components/common';
 import LoginForm from './components/LoginForm';
-class App extends Component{
 
+class App extends Component{
+componentWillMount(){
+  
+ var app = firebase.initializeApp({
+  });
+}
 
   render(){
     return(
--- src/components/LoginForm.js
+++ src/components/LoginForm.js
@@ -1,12 +1,24 @@
 import React, { Component } from 'react'; 
+import { Text } from 'react-native'; 
+import * as firebase from 'firebase';
 import { Button, Card, CardSection, Input } from './common'; 
 
-class LoginForm extends Component{
-  state={ email: '', password: ''};
 
-  onButtonPress(){
-    
-  }
+class LoginForm extends Component{
+ 
+
+  state={ email: '', password: '', error: ''};
+onButtonPress(){
+  const {email,password} =this.state;
+  firebase.auth().signInWithEmailAndPassword(email,password)
+  .catch(()=>{
+    firebase.auth().createUserWithEmailAndPassword(email,password)
+    .catch(()=>{
+      this.setState({error:'Authentication failed.'});
+    });
+  });
+}
+  
 render (){
   return(
     <Card>
@@ -17,13 +29,25 @@ render (){
       <CardSection>
         <Input label="Password" secureTextEntry placeholder = "password" value = {this.state.password} onChangeText={password => this.setState({password})} /> 
       </CardSection>
+
+      <Text style = {styles.errorTextStyle} >
+        {this.state.error}
+        </Text>
       <CardSection>
-        <Button onPress={this.onButtonPress.bind(this)}>
+        <Button onPress={this.onButtonPress.bind(this)} >
           Log In
           </Button>
         </CardSection>
       </Card>
   );
 }
+}
+
+const styles = {
+  errorTextStyle: {
+    fontSize:20, 
+    alignSelf:'center', 
+    color: 'red'
+  }
 }
 export default LoginForm;
