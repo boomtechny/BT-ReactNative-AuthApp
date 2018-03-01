0e01209ccbcc16cf5fefd9de04e511f4369f51cd
BoomTech <boomtechny@gmail.com>
2018-03-01 14:18:50 -0500

    Added ActivitySpinner component and configured state to update login form appropriately during authentication user flow. Also migrated sensitive firebase api data to constants file

--- src/App.js
+++ src/App.js
@@ -3,11 +3,18 @@ import {View} from 'react-native';
 import * as firebase from 'firebase';
 import {Header} from './components/common';
 import LoginForm from './components/LoginForm';
+import  { FB_APIKEY, FB_AUTHDOMAIN, FB_DATABASEURL, FB_PROJECTID, FB_STORAGEBUCKET, FB_MESSAGINGSENDERID} from '../config/constants.js';
 
 class App extends Component{
 componentWillMount(){
   
  var app = firebase.initializeApp({
+    apiKey: FB_APIKEY,
+    authDomain: FB_AUTHDOMAIN,
+    databaseURL: FB_DATABASEURL,
+    projectId: FB_PROJECTID,
+    storageBucket: FB_STORAGEBUCKET,
+    messagingSenderId: FB_MESSAGINGSENDERID
   });
 }
 
